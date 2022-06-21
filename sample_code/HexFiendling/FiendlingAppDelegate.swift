1bf3200d7591a8c9b7536ca1c574a83b43c190fc
Kevin Wojniak <kainjow@users.noreply.github.com>
2022-06-20 22:31:31 -0700

    setUpInMemoryHexViewIntoView

--- sample_code/HexFiendling/FiendlingAppDelegate.swift
+++ sample_code/HexFiendling/FiendlingAppDelegate.swift
@@ -22,6 +22,10 @@ class FiendlingAppDelegate2: NSObject, NSApplicationDelegate {
     @IBOutlet var boundDataTextView: HFTextView?
     @objc dynamic var textViewBoundData = Data()
 
+    /**** SECOND TAB ****/
+    var inMemoryController: HFController?
+    var fileController: HFController?
+
     /**** THIRD TAB ****/
     @IBOutlet var externalDataTextView: NSTextView?
     @objc dynamic var externalData = Data()
@@ -31,9 +35,7 @@ class FiendlingAppDelegate2: NSObject, NSApplicationDelegate {
 
     var rtfSampleData: Data {
         get throws {
-            guard let url = Bundle.main.url(forResource: "sampleData.rtf", withExtension: nil) else {
-                fatalError()
-            }
+            guard let url = Bundle.main.url(forResource: "sampleData.rtf", withExtension: nil) else { fatalError() }
             return try Data(contentsOf: url)
         }
     }
@@ -42,6 +44,44 @@ class FiendlingAppDelegate2: NSObject, NSApplicationDelegate {
         /* Bind our text view to our bound data */
         boundDataTextView?.bind(NSBindingName("data"), to: self, withKeyPath: "textViewBoundData")
         setValue(try rtfSampleData, forKey: "textViewBoundData")
+    }
+
+    func setUpInMemoryHexViewIntoView(containerView: NSView) {
+        /* Get some random data to display */
+        let dataSize = 1024
+        guard let data = NSMutableData(length: dataSize) else { fatalError() }
+        let fd = open("/dev/random", O_RDONLY)
+        read(fd, data.mutableBytes, dataSize)
+        close(fd)
+
+        /* Make a controller to hook everything up, and then configure it a bit. */
+        inMemoryController = HFController()
+        inMemoryController?.bytesPerColumn = 4
+
+        /* Put that data in a byte slice.  Here we use initWithData:, which causes the byte slice to take ownership of the data (and may modify it).  If we want to prevent our data from being modified, we would use initWithUnsharedData: */
+        let byteSlice = HFSharedMemoryByteSlice(data: data)
+        let byteArray = HFBTreeByteArray()
+        byteArray.insertByteSlice(byteSlice, in: HFRangeMake(0, 0))
+        inMemoryController?.byteArray = byteArray
+
+        /* Make an HFHexTextRepresenter. */
+        let hexRep = HFHexTextRepresenter()
+        hexRep.rowBackgroundColors = [] //An empty array means don't draw a background.
+        inMemoryController?.addRepresenter(hexRep)
+
+        /* Grab its view and stick it into our container. */
+        let hexView = hexRep.view()
+        hexView.frame = containerView.bounds
+        hexView.autoresizingMask = [.width, .height]
+        containerView.addSubview(hexView)
+    }
+
+    func viewForIdentifier(_ ident: String) -> NSView {
+        guard let tabView = tabView else { fatalError() }
+        let index = tabView.indexOfTabViewItem(withIdentifier: ident)
+        if index == NSNotFound { fatalError() }
+        guard let view = tabView.tabViewItem(at: index).view else { fatalError() }
+        return view
     }
 
     func applicationDidFinishLaunching(_ notification: Notification) {
@@ -53,7 +93,7 @@ class FiendlingAppDelegate2: NSObject, NSApplicationDelegate {
         ]
         do {
             try setUpBoundDataHexView()
-            //[self setUpInMemoryHexViewIntoView:[self viewForIdentifier:@"in_memory_hex_view"]];
+            setUpInMemoryHexViewIntoView(containerView: viewForIdentifier("in_memory_hex_view"))
             //[self setUpFileMultipleViewIntoView:[self viewForIdentifier:@"file_data_multiple_views"]];
             //[self setUpExternalDataView:[self viewForIdentifier:@"external_data"]];
         } catch {
