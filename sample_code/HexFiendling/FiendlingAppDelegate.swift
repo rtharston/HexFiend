ef7041aa828b6e8d637ed807c5c510db002b3312
Kevin Wojniak <kainjow@users.noreply.github.com>
2022-06-21 07:08:50 -0700

    setUpFileMultipleViewIntoView

--- sample_code/HexFiendling/FiendlingAppDelegate.swift
+++ sample_code/HexFiendling/FiendlingAppDelegate.swift
@@ -76,6 +76,45 @@ class FiendlingAppDelegate2: NSObject, NSApplicationDelegate {
         containerView.addSubview(hexView)
     }
 
+    func setUpFileMultipleViewIntoView(containerView: NSView) throws {
+        /* We're going to show the contents of our Info.plist */
+        let infoplist = Bundle.main.bundleURL.appendingPathComponent("Contents/Info.plist")
+        let reference = try HFFileReference(path: infoplist.path)
+
+        /* Make a controller to hook everything up, and then configure it a bit. */
+        fileController = HFController()
+        fileController?.bytesPerColumn = 1
+
+        /* Put our data in a byte slice. */
+        let byteSlice = HFFileByteSlice(file: reference)
+        let byteArray = HFBTreeByteArray()
+        byteArray.insertByteSlice(byteSlice, in: HFRangeMake(0, 0))
+        fileController?.byteArray = byteArray
+
+        /* Here we're going to make three representers - one for the hex, one for the ASCII, and one for the scrollbar.  To lay these all out properly, we'll use a fourth HFLayoutRepresenter. */
+        let layoutRep = HFLayoutRepresenter()
+        let hexRep = HFHexTextRepresenter()
+        let asciiRep = HFStringEncodingTextRepresenter()
+        let scrollRep = HFVerticalScrollerRepresenter()
+
+        /* Add all our reps to the controller. */
+        fileController?.addRepresenter(layoutRep)
+        fileController?.addRepresenter(hexRep)
+        fileController?.addRepresenter(asciiRep)
+        fileController?.addRepresenter(scrollRep)
+
+        /* Tell the layout rep which reps it should lay out. */
+        layoutRep.addRepresenter(hexRep)
+        layoutRep.addRepresenter(asciiRep)
+        layoutRep.addRepresenter(scrollRep)
+
+        /* Grab the layout rep's view and stick it into our container. */
+        let layoutView = layoutRep.view()
+        layoutView.frame = containerView.bounds
+        layoutView.autoresizingMask = [.width, .height]
+        containerView.addSubview(layoutView)
+    }
+
     func viewForIdentifier(_ ident: String) -> NSView {
         guard let tabView = tabView else { fatalError() }
         let index = tabView.indexOfTabViewItem(withIdentifier: ident)
@@ -94,7 +133,7 @@ class FiendlingAppDelegate2: NSObject, NSApplicationDelegate {
         do {
             try setUpBoundDataHexView()
             setUpInMemoryHexViewIntoView(containerView: viewForIdentifier("in_memory_hex_view"))
-            //[self setUpFileMultipleViewIntoView:[self viewForIdentifier:@"file_data_multiple_views"]];
+            try setUpFileMultipleViewIntoView(containerView: viewForIdentifier("file_data_multiple_views"))
             //[self setUpExternalDataView:[self viewForIdentifier:@"external_data"]];
         } catch {
             NSApp.presentError(error)
